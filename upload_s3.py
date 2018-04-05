943d8d4aabd9035264ac0ac607fb43620b2d44d7
sriharshavaleti <38095749+sriharshavaleti@users.noreply.github.com>
2018-04-06 00:54:12 +0530

    Update upload_s3.py

--- upload_s3.py
+++ upload_s3.py
@@ -8,7 +8,7 @@ path = sys.argv[1]
 s3 = boto.resource('s3') # This is using IAM role other wise you need to pass Access key and secret access key
 filename = os.path.basename(path)
 with open(path,'rb') as file_content:
-    content_type = mimetype.guess_type(path)
+    content_type = mimetype.guess_type(path) # used to find the type of file
     s3.Object(bucket_name, key+filename).put(ContentType = content_type[0],Body = file_content,ACL = 'public-read')
     print("File uploaded succesfully")
 
