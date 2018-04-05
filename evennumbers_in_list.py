d798133078a3a3fff8f9caf57dd6c5c988703026
sriharshavaleti <38095749+sriharshavaleti@users.noreply.github.com>
2018-04-06 00:53:22 +0530

    Update evennumbers_in_list.py

--- evennumbers_in_list.py
+++ evennumbers_in_list.py
@@ -1,4 +1,4 @@
 import numpy as np
 x = [1,6,3,4,8,9,4,2,5,7,60,10]
-y = [i for i in x if np.mod(i,2)==0]
+y = [i for i in x if np.mod(i,2)==0] or [i for i in x if i%2==0] # we can use both the methods
 print y
