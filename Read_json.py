012f3707fd069f873e19cf123da4aafc847a4477
sriharshavaleti <38095749+sriharshavaleti@users.noreply.github.com>
2018-04-05 23:31:10 +0530

    Create Read_json.py

---
+++ Read_json.py
@@ -0,0 +1,13 @@
+import json
+import os
+import sys
+
+json_data = sys.argv[1]
+filename=os.path.basename(localpath)
+with open(filename, 'r') as f:
+    datastore = json.load(f)
+    print(datastore)
+for i in datastore
+     for k in json_data
+          if datastore[i] =! json_data[j]
+               print(datastore[i])
