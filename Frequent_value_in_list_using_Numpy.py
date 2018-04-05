dd32d2777f489e44558b78788ab13fe579aed32d
sriharshavaleti <38095749+sriharshavaleti@users.noreply.github.com>
2018-04-06 00:52:07 +0530

    Update Frequent_value_in_list_using_Numpy.py

--- Frequent_value_in_list_using_Numpy.py
+++ Frequent_value_in_list_using_Numpy.py
@@ -1,6 +1,6 @@
 import numpy as np
 x = np.array([1,2,2,2,3], np.int32)
-print ("Input Array Vales")
+print ("Input Array Vales") #input values
 print(x)
 print ("Most frequent values in array")
-print(np.bincount(x).argMax(1))
+print(np.bincount(x).argMax(1)) # 2 appears most frequent
