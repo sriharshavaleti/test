08cb3ae4616497649fc2c22399510f357121fdac
sriharshavaleti <38095749+sriharshavaleti@users.noreply.github.com>
2018-04-06 00:23:49 +0530

    Update Count_no_of_integers_list.py

--- Count_no_of_integers_list.py
+++ Count_no_of_integers_list.py
@@ -1,4 +1,4 @@
 from collections import Counter
-list = [1, 6, 201,8, 21, 10, 21, 21, 6, 2, 8, 6, 1]
-c = Counter(list)
-print (c)
+list = [1, 6, 201,8, 21, 10, 21, 21, 6, 2, 8, 6, 1] #input values 
+c = Counter(list) 
+print (c) # displayes the Counting of values in Assending order
